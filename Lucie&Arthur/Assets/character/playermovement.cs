a805cd27ced22cd0953cf12973e310ac233e3a50
Everpaly <arthur.lerra@gmail.com>
2023-04-17 18:47:55 +0200

    added animation + sound when walking

--- Lucie&Arthur/Assets/character/playermovement.cs
+++ Lucie&Arthur/Assets/character/playermovement.cs
@@ -8,6 +8,9 @@ public class playermovement : MonoBehaviour
     public Rigidbody2D rb;
     Vector2 movement;
     public Animator animator;
+    bool ismoving = false;
+    public AudioSource audioSrc;
+    bool isongrass = false;
 
     void Update()
     {
@@ -17,10 +20,43 @@ public class playermovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("speed", movement.sqrMagnitude);
+        
+        if (movement.sqrMagnitude >= 0.01f)
+        {
+            ismoving = true; 
+        }
+        else
+        {
+            ismoving = false;
+        } 
+        
+
+        if(ismoving == true)
+        {
+            if (!audioSrc.isPlaying)
+            {
+                audioSrc.Play();                
+            }           
+            
+        }
+        else
+        {
+            audioSrc.Stop();            
+        }
+
+
+
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * movementspeed * Time.fixedDeltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(gameObject.tag == "grass")
+        {
+            isongrass = true;
+        }
     }
 }
